23a86ac4ac713612d34186670dc33a5a469733fc
Min Hee  Park <minheekangg@gmail.com>
2018-10-18 19:23:34 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -34,7 +34,7 @@ HAVING SUM(pledges.amount) - projects.funding_goal  >= 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT users.names SUM(pledges.amount)
+"SELECT users.name, SUM(pledges.amount)
 FROM users
 JOIN pledges
 ON users.id = pledges.user_id
